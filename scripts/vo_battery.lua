efb57c772bce9185f80eec7372674688c7762880
Xingan Wang <wangxgwxg@gmail.com>
2016-06-25 13:04:55 +0800

    add some configs and change vo

--- scripts/vo_battery.lua
+++ scripts/vo_battery.lua
@@ -1,6 +1,6 @@
 -- If the laptop is on battery, the VO set in the config will be choosen,
 -- else the one defined with „hqvo“ is used.
-local hqvo = "opengl-hq:dither-depth=auto:gamma-auto:interpolation:correct-downscaling=no:icc-profile-auto:icc-cache-dir=~/.config/mpv/tmp/icc-cache"
+local hqvo = "vo=opengl-hq:scale=ewa_lanczossharp:cscale=ewa_lanczossoft:dscale=mitchell:tscale=triangle:scale-antiring=0.8:cscale-antiring=0.9:dither-depth=auto:target-prim=bt.709:correct-downscaling=yes"
 local utils = require 'mp.utils'
 if mp.get_property_bool("option-info/vo/set-from-commandline") == true then
     return
