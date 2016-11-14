e3bf2272a4fdb6c44e6dcfbc586e4acda3c88bb0
Xingan Wang <wangxgwxg@gmail.com>
2016-11-13 22:25:49 -0800

    update to new mpv release

--- scripts/vo_battery.lua
+++ scripts/vo_battery.lua
@@ -1,6 +1,5 @@
 -- If the laptop is on battery, the VO set in the config will be choosen,
 -- else the one defined with „hqvo“ is used.
-local hqvo = "opengl-hq"
 local utils = require 'mp.utils'
 if mp.get_property_bool("option-info/vo/set-from-commandline") == true then
     return
@@ -9,7 +8,7 @@ t = {}
 t.args = {"/usr/bin/pmset", "-g", "ac"}
 res = utils.subprocess(t)
 if res.stdout ~= "No adapter attached.\n" then
-    mp.set_property("options/vo", hqvo)
+    mp.set_property("options/profile", 'opengl-hq')
     -- to activate interpolation
     -- mp.set_property("options/video-sync", "display-resample")
 end
